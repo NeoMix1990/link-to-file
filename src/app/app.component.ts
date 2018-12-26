9bb4cae965a58121b4ad086d9540cef3dc088326
Yurii Sologub <y.sologub@mobimill.com>
2018-12-26 10:21:44 +0200

    hi mf

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'fileUrl';
+export class AppComponent implements OnInit {
+
+  imgFileData64;
+  ngOnInit() {
+    const toDataURL = url => fetch(url)
+    .then(response => response.blob())
+    .then(blob => new Promise((resolve, reject) => {
+      const reader = new FileReader()
+      reader.onloadend = () => resolve(reader.result)
+      reader.onerror = reject
+      reader.readAsDataURL(blob)
+    }));
+
+
+  toDataURL('http://1.gravatar.com/avatar/767fc9c115a1b989744c755db47feb60?size=800')
+    .then(dataUrl => {
+      console.log('RESULT:', dataUrl);
+      this.imgFileData64 = dataUrl;
+    })
+    }
 }
